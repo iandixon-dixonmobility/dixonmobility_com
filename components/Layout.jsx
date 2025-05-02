697cf5e881bd20dce01f3e383d0a5ee4518e3f03
Ian Dixon <ian@dixonmobility.com>
2025-05-02 09:41:06 -0400

    Updated Meta description.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
         <>
             <Head>
                 <title>Dixon Mobility</title>
-                <meta name='description' content='Blinder making it simple for you to build and grow your SaaS applications, or any business idea' />
+                <meta name='description' content='Dixon Mobility develops great custom mobile app for public transit and more.' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
