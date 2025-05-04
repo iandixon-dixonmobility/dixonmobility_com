9de9e805843be0da9e2b1ff04f03d7259cbf113d
Ian Dixon <ian@dixonmobility.com>
2025-05-03 23:20:44 -0400

    Updating content.

--- components/ui/CTA/index.jsx
+++ components/ui/CTA/index.jsx
@@ -16,7 +16,7 @@ const CTA = () => {
                             Leader In Public Transit
                         </h2>
                         <p className="mt-3 text-gray-600">
-                            We have been publishing public transit apps for riders since 2012, and with over <b>5 million downloads</b> in the Apple App Store and Google Play Store, Dixon Mobility is proven leaders in mobile public transit. Our success is built around tailoring each app for the market it serves and the unique characteristics of the transit system.
+                            We have been publishing public transit apps for riders since 2012, and with over <b>5 million downloads</b> in the Apple App Store and Google Play Store, Dixon Mobility is a proven leader in mobile public transit. Our success is built around tailoring each app for the market it serves and the unique characteristics of the transit system.
                         </p>
                         <NavLink
                             href="/get-started"
